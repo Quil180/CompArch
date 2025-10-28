ae3a2cb899fcc4848670352067e71527ce83d0c0
Quil <quil180gaming@gmail.com>
2025-10-27 21:09:22 -0400

    updated

---
+++ Projects/project1/build/CMakeFiles/project1.dir/link.d
@@ -0,0 +1,94 @@
+project1: \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/Scrt1.o \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/crti.o \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbeginS.o \
+  CMakeFiles/project1.dir/src/main.cpp.o \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libstdc++.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so.6 \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libmvec.so.1 \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so.6 \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc_nonshared.a \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/ld-linux-x86-64.so.2 \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so \
+  /nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so.1 \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a \
+  /nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtendS.o \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/crtn.o \
+  /nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/ld-linux-x86-64.so.2
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/Scrt1.o:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/crti.o:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtbeginS.o:
+
+CMakeFiles/project1.dir/src/main.cpp.o:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libstdc++.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libm.so.6:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libmvec.so.1:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc.so.6:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/libc_nonshared.a:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/ld-linux-x86-64.so.2:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so:
+
+/nix/store/z7a34j3xnp66rpddayyxrxwsahxccbip-gcc-14.3.0-lib/lib/libgcc_s.so.1:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/libgcc.a:
+
+/nix/store/ffrg0560kj0066s4k9pznjand907nlnz-gcc-14.3.0/lib/gcc/x86_64-unknown-linux-gnu/14.3.0/crtendS.o:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/crtn.o:
+
+/nix/store/daamdpmaz2vjvna55ccrc30qw3qb8h6d-glibc-2.40-66/lib/ld-linux-x86-64.so.2:
--- Projects/project1/src/main.cpp
+++ Projects/project1/src/main.cpp
@@ -1,10 +1,82 @@
+/*
+ * Made by Yousef Awad in C++ using CMake for the compilation.
+ * Yay!
+*/
+
+/*
+ * The following is all of the return types and what they signify:
+ * 0: Program ran succesfully
+ * 1: printf error in main
+ * 
+*/
+
+#include <fstream>
+#include <cstdio>
 #include <iostream>
+
+#define NUM_SETS 1
+#define ASSOC 1
+#define BLOCK_SIZE 64
+
+long long int tag[NUM_SETS][ASSOC];
+long long int lru_pos[NUM_SETS][ASSOC];
+bool dirty[NUM_SETS][ASSOC];
+
+// statistic variables
+unsigned int hit  = 0;
+unsigned int miss = 0;
+
+void update_lru(long long int add)
+{
+}
+
+void update_fifo(long long int add)
+{
+}
+
+void simulate_access(char opcode, long long int add)
+{
+  int set = (add / BLOCK_SIZE) % NUM_SETS;
+  long long int tag_accessing = add / BLOCK_SIZE;
+
+  for (int i = 0; i < ASSOC; i++)
+  {
+    if (tag_accessing == tag[set][i])
+    {
+      // Cache hit!!
+      hit++;
+      if (/* LRU policy goes here */)
+      {
+        update_lru(add);
+      }
+      else
+      {
+        update_fifo(add);
+      }
+    }
+    else
+    {
+      // Cache Miss!
+      miss++;
+    }
+  }
+}
 
 int main(void)
 {
-  bool test = true;
-  if (test)
-  {
-    std::cout << "test" << std::endl;
-  }
+  char opcode;
+  long long int add;
+
+  // Opening file as an input/read
+  std::ofstream file;
+  file.open("/* path goes here */", std::ios::in);
+  // Reading the file inputs
+
+  // Closing the file
+  file.close();
+
+  std::cout << "Hits: " << hit << std::endl;
+  std::cout << "Miss: " << miss << std::endl;
+
+  return 0;
 }
